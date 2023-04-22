764054b82631292893dbd436f8235d01d2e4c5eb
Andrew Liauba <Iamliauba@hotmail.com>
2023-04-22 18:02:47 -0400

    Initial Commit

---
+++ MCQ/MCQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCQApp.swift
+//  MCQ
+//
+//  Created by andrew liauba on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct MCQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
